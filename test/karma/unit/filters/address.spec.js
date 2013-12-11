c1f788188065c1e867e838da4e06c76f11ca1ded
Nicola Sanitate <n.sanitate@gmail.com>
2013-12-11 10:22:47 +0100

    Aggiunte funzionalità di aggiunta e rimozione di un plesso.

---
+++ test/karma/unit/filters/address.spec.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  describe('Aurea filters', function () {
+
+    describe('Address', function () {
+
+      // load the filter's module
+      beforeEach(module('aurea'));
+
+      // initialize a new instance of the filter before each test
+      var address;
+      beforeEach(inject(function ($filter) {
+        address = $filter('address');
+      }));
+
+      it('should return the address well formatted', function () {
+        var obj = {
+          address: 'via Manzoni s.n.',
+          zipCode: '70016',
+          city: 'Noicattaro',
+          province: 'BA'
+        };
+
+        expect(address(obj)).toBe('via Manzoni s.n. 70016 Noicattaro BA');
+      });
+
+    });
+
+  });
+
+})();
