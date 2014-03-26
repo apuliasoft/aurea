fb3d986f9f48b858d93fee334a4c52594d0f9c53
Francesco Pavone <ciccio1986@gmail.com>
2014-03-26 18:04:14 +0100

    Aggiunto il modulo mailer per l'invio di mail stramite protocollo SMTP. Fix #22.

--- config/env/test.js
+++ config/env/test.js
@@ -30,5 +30,16 @@ module.exports = {
         clientID: 'API_KEY',
         clientSecret: 'SECRET_KEY',
         callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    },
+    mailer: {
+        host: '',
+        port: 25,
+        secureConnection: false,
+        name: 'Aurea-test',
+        auth: {
+            user: '',
+            pass: ''
+        },
+        maxConnections: 5
     }
 };
