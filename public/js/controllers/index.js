da26ff2ee9b6da7aea9ebca918fe6fd6615b0ecb
Nicola Sanitate <n.sanitate@gmail.com>
2014-08-23 18:42:34 +0200

    Rivista la grafica

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -3,5 +3,5 @@
 angular.module('aurea.system').controller('IndexCtrl', ['$scope', 'AcademicYear', 'Global', function ($scope, AcademicYear, Global) {
     $scope.global = Global;
 
-
+    Global.title = "Aurea: un registro elettronico aperto";
 }]);
--- public/js/controllers/schools.js
+++ public/js/controllers/schools.js
@@ -3,6 +3,11 @@
 angular.module('aurea.schools').controller('SchoolsCtrl', ['$scope', '$stateParams', '$location', '_', 'Global', 'School', function ($scope, $stateParams, $location, _, Global, School) {
     $scope.global = Global;
 
+    Global.title = 'Aurea';
+    Global.subtitle = 'Scuole';
+
+    Global.actions = [{ name: 'Aggiungi', func: $scope.goToCreateSchool}];
+
     $scope.goToListSchools = function () {
         $location.path('scuole');
     };
@@ -26,6 +31,8 @@ angular.module('aurea.schools').controller('SchoolsCtrl', ['$scope', '$statePara
 
     $scope.init = function () {
         $scope.school = new School();
+        Global.title = 'Scuole';
+        Global.subtitle = 'Nuova';
     };
 
     $scope.create = function (isValid) {
@@ -66,7 +73,12 @@ angular.module('aurea.schools').controller('SchoolsCtrl', ['$scope', '$statePara
     };
 
     $scope.findOne = function () {
-        $scope.school = School.get({schoolId: $stateParams.schoolId});
+        School.get({schoolId: $stateParams.schoolId}).$promise
+            .then(function(school){
+                $scope.school = school;
+                Global.title = 'Scuole';
+                Global.subtitle = school.name;
+            });
     };
 
 }]);
