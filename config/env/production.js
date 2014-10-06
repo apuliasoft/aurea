f1d604da844aa66afb018c8dee24e73cc520d8c6
Giuseppe Santoro <giuseppesantoro87@gmail.com>
2014-10-06 13:12:55 +0200

    rimosse le configurazioni per i social logins§

--- config/env/production.js
+++ config/env/production.js
@@ -5,31 +5,6 @@ module.exports = {
     app: {
         name: 'Aurea: un registro elettronico aperto - Production'
     },
-    facebook: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/facebook/callback'
-    },
-    twitter: {
-        clientID: 'CONSUMER_KEY',
-        clientSecret: 'CONSUMER_SECRET',
-        callbackURL: 'http://localhost:3000/auth/twitter/callback'
-    },
-    github: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/github/callback'
-    },
-    google: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/google/callback'
-    },
-    linkedin: {
-        clientID: 'API_KEY',
-        clientSecret: 'SECRET_KEY',
-        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
-    },
     mailer: {
         host: '',
         port: 25,
