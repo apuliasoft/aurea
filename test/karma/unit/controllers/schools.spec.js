2b93bcae54334eaadf03021e4e4fb4b772f80c55
Nicola Sanitate <n.sanitate@gmail.com>
2014-11-16 23:46:07 +0100

    Prima revisione dei test

--- test/karma/unit/controllers/schools.spec.js
+++ test/karma/unit/controllers/schools.spec.js
@@ -1,17 +1,17 @@
 'use strict';
 
-(function() {
+(function () {
     // Schools Controller Spec
-    describe('Aurea controllers', function() {
-        describe('SchoolsCtrl', function() {
+    describe('Aurea controllers', function () {
+        describe('SchoolsCtrl', function () {
             // The $resource service augments the response object with methods for updating and deleting the resource.
             // If we were to use the standard toEqual matcher, our tests would fail because the test values would not match
             // the responses exactly. To solve the problem, we use a newly-defined toEqualData Jasmine matcher.
             // When the toEqualData matcher compares two objects, it takes only object properties into
             // account and ignores methods.
-            beforeEach(function() {
-                this.addMatchers({
-                    toEqualData: function(expected) {
+            beforeEach(function () {
+                jasmine.addMatchers({
+                    toEqualData: function (expected) {
                         return angular.equals(this.actual, expected);
                     }
                 });
@@ -23,20 +23,23 @@
             // Initialize the controller and a mock scope
             var SchoolsCtrl,
                 scope,
-                $httpBackend,
+                $state,
                 $stateParams,
+                $httpBackend,
                 $location;
 
             // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
             // This allows us to inject a service but then attach it to a variable
             // with the same name as the service.
-            beforeEach(inject(function($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_) {
+            beforeEach(inject(function ($controller, $rootScope, _$state_, _$stateParams_, _$httpBackend_, _$location_) {
 
                 scope = $rootScope.$new();
 
                 SchoolsCtrl = $controller('SchoolsCtrl', {
                     $scope: scope
                 });
+
+                $state = _$state_;
 
                 $stateParams = _$stateParams_;
 
@@ -46,72 +49,69 @@
 
             }));
 
-            it('$scope.list() should locate to list schools URL', function() {
+            afterEach(function() {
+                $httpBackend.verifyNoOutstandingExpectation();
+                $httpBackend.verifyNoOutstandingRequest();
+            });
+
+            it('$scope.goToListSchools() should locate to list schools URL', function () {
 
                 // run controller
-                scope.list();
+                scope.goToListSchools();
 
                 // test URL location to list
                 expect($location.path()).toBe('/scuole');
             });
 
-            it('$scope.new() should locate to create school form URL', function() {
+            it('$scope.goToCreateSchool() should locate to create school form URL', function () {
 
                 // run controller
-                scope.new();
+                scope.goToCreateSchool();
 
                 // test URL location to create form
                 expect($location.path()).toBe('/scuole/crea');
             });
 
-            it('$scope.view() should locate to view school URL', function() {
-
-                // fixture object to view
-                var viewSchoolData = function () {
-                    return {
-                        _id: '525cf20451979dea2c000001'
-                    };
-                };
-
-                // run controller
-                scope.view(viewSchoolData());
-
-                // test URL location to view
-                expect($location.path()).toBe('/scuole/' + viewSchoolData()._id);
-            });
-
-            it('$scope.edit() should locate to edit school form URL', function() {
+            it('$scope.goToEditSchool() should locate to edit school form URL', inject(function (School) {
 
                 // fixture object to edit
-                var editSchoolData = function () {
-                    return {
-                        _id: '525cf20451979dea2c000001'
-                    };
-                };
+                var school = new School({
+                    _id: '1234567890abcdef12345678'
+                });
 
                 // run controller
-                scope.edit(editSchoolData());
+                scope.edit(school);
 
                 // test URL location to view
-                expect($location.path()).toBe('/scuole/' + editSchoolData()._id + '/modifica');
-            });
+                expect($location.path()).toBe('/scuole/' + school._id);
+            }));
+
+            it('$scope.goToEditSchool() should locate to list complexes form URL', inject(function (School) {
+
+                // fixture object to edit
+                var school = new School({
+                    _id: '1234567890abcdef12345678'
+                });
+
+                // run controller
+                scope.edit(school);
+
+                // test URL location to view
+                expect($location.path()).toBe('/scuole/' + school._id);
+            }));
 
             it('$scope.find() should create an array with at least one school object ' +
-                'fetched from XHR', function() {
+                'fetched from XHR', function () {
 
                 // fixture expected GET request
-                var responseSchoolData = function() {
-                    return [{
-                        _id: '1234567890abcdef12345678',
-                        name: 'Istituto Tecnico',
-                        complex: [{
-                            street: 'Via Qualunque 1',
-                            zipCode: '12345',
-                            city: 'Chissadove',
-                            province: 'AZ'
-                        }],
-                        __v: 0
-                    }];
+                var responseSchoolData = function () {
+                    return [
+                        {
+                            _id: '1234567890abcdef12345678',
+                            name: 'Istituto Tecnico',
+                            __v: 0
+                        }
+                    ];
                 };
 
                 // test GET happens correctly
@@ -126,8 +126,23 @@
 
             });
 
-            it('$scope.findOne() should create an array with one school object fetched ' +
-                'from XHR using a schoolId URL parameter', function () {
+            it('$scope.init() should create a void school object', function () {
+
+                // set edit mode to false
+                $state.current.data.editMode = false;
+
+                // run controller
+                scope.init();
+
+                // test scope value
+                expect(!!scope.school).toBe(true);
+            });
+
+            it('$scope.findOne() should create a school object ' +
+                'fetched from XHR using a schoolId URL parameter', function () {
+
+                // set edit mode to true
+                $state.current.data.editMode = true;
 
                 // fixture URL parament
                 $stateParams.schoolId = '1234567890abcdef12345678';
@@ -137,12 +152,6 @@
                     return {
                         _id: '1234567890abcdef12345678',
                         name: 'Istituto Tecnico',
-                        complex: [{
-                            street: 'Via Qualunque 1',
-                            zipCode: '12345',
-                            city: 'Chissadove',
-                            province: 'AZ'
-                        }],
                         __v: 0
                     };
                 };
@@ -152,7 +161,7 @@
                 $httpBackend.expectGET(urlRegex).respond(responseSchoolData());
 
                 // run controller
-                scope.findOne();
+                scope.init();
                 $httpBackend.flush();
 
                 // test scope value
@@ -160,66 +169,15 @@
 
             });
 
-            it('$scope.init() should create an array with one void school object', function() {
+            it('$scope.save() should create a new valid school', inject(function (School) {
 
-                // run controller
-                scope.init();
-
-                // test scope value
-                expect(!!scope.school).toBe(true);
-            });
-
-            it('$scope.addComplex should create a void complex object ' +
-                'and add it to the school in the scope', inject(function (School) {
-
-                // init school in scope
-                scope.school = new School({complexes:[]});
-
-                // Run controller
-                scope.addComplex();
-
-                // test after successful adding
-                expect(scope.school.complexes.length).toBe(1);
-            }));
-
-            it('$scope.removeComplex should delete a complex object ' +
-                'from the school in the scope', inject(function (School) {
-
-                var deleteComplexData = function () {
-                    return {
-                        street: 'Via Qualunque 1',
-                        zipCode: '12345',
-                        city: 'Chissadove',
-                        province: 'AZ'
-                    };
-                };
-
-                // init school in scope
-                scope.school = new School();
-                scope.school.complexes = [];
-                scope.school.complexes.push(deleteComplexData());
-
-                // Run controller
-                scope.removeComplex(deleteComplexData());
-
-                // test after successful delete
-                expect(scope.school.complexes.length).toBe(0);
-            }));
-
-            it('$scope.create() with valid form data should send a POST request ' +
-                'with the form input values and then ' +
-                'locate to new object URL', inject(function (School) {
+                // set edit mode to false
+                $state.current.data.editMode = false;
 
                 // fixture to send in POST request
                 var postSchoolData = function () {
                     return {
-                        name: 'Istituto Tecnico',
-                        complex: [{
-                            street: 'Via Qualunque 1',
-                            zipCode: '12345',
-                            city: 'Chissadove',
-                            province: 'AZ'
-                        }]
+                        name: 'Istituto Tecnico'
                     };
                 };
 
@@ -228,12 +186,6 @@
                     return {
                         _id: '1234567890abcdef12345678',
                         name: 'Istituto Tecnico',
-                        complex: [{
-                            street: 'Via Qualunque 1',
-                            zipCode: '12345',
-                            city: 'Chissadove',
-                            province: 'AZ'
-                        }],
                         __v: 0
                     };
                 };
@@ -245,44 +197,37 @@
                 $httpBackend.expectPOST('schools', postSchoolData()).respond(responseSchoolData());
 
                 // Run controller
-                scope.create();
+                scope.save();
                 $httpBackend.flush();
 
                 // test form input(s) are reset
-                expect(scope.school).toEqualData(new School({complexes:[{}]}));
+                expect(scope.school).toEqualData(new School({complexes: [
+                    {}
+                ]}));
 
                 // test URL location to new object
-                expect($location.path()).toBe('/scuole/' + responseSchoolData()._id);
+                expect($location.path()).toBe('/scuole');
             }));
 
-            it('$scope.update() should update a valid school', inject(function (School) {
+            it('$scope.save() should update a valid school', inject(function (School) {
+
+                // set edit mode to true
+                $state.current.data.editMode = true;
 
                 // fixture to send in PUT request
                 var putSchoolData = function () {
                     return {
                         _id: '1234567890abcdef12345678',
                         name: 'Istituto Tecnico',
-                        complex: [{
-                            street: 'Via Qualunque 1',
-                            zipCode: '12345',
-                            city: 'Chissadove',
-                            province: 'AZ'
-                        }],
                         __v: 0
                     };
                 };
 
                 // fixture expected from PUT request
-                var responseSchoolData = function() {
+                var responseSchoolData = function () {
                     return {
                         _id: '1234567890abcdef12345678',
                         name: 'Liceo Scientifico',
-                        complex: [{
-                            street: 'Via Qualunque altra 1',
-                            zipCode: '54321',
-                            city: 'Chissadove altro',
-                            province: 'ZA'
-                        }],
                         __v: 1
                     };
                 };
@@ -298,16 +243,15 @@
                 $httpBackend.expectPUT(urlRegex).respond(responseSchoolData());
 
                 // run controller
-                scope.update();
+                scope.save();
                 $httpBackend.flush();
 
                 // test URL location to new object
-                expect($location.path()).toBe('/scuole/' + responseSchoolData()._id);
+                expect($location.path()).toBe('/scuole');
 
             }));
 
-            it('$scope.remove() should send a DELETE request with a valid schoolId' +
-                'and remove the school from the scope', inject(function (School) {
+            it('$scope.remove() should delete a school', inject(function (School) {
 
                 // fixture to send in DELETE request
                 var school = new School({
